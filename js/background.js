fb24d650f4b38ada863187a34ed83e8579608a65
Lands <380788433@qq.com>
2019-09-09 09:34:10 +0800

    增加模块：滑块筛选图片大小

--- js/background.js
+++ js/background.js
@@ -1,7 +1,6 @@
 function load() {
   window.console.info('background.js load！');
   if(window.$){
-    console.log("$.fn.flexImages：", window.$.fn.flexImages);
     init();
   }
 }
@@ -12,8 +11,32 @@ function init() {
   let tab = get("tab");
   let images = get("images");
   let container = $(".container");
+  $(".width-range-slider").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 2000,
+    from: 0,
+    to: 2000,
+    postfix: "px"
+  });
+  $(".height-range-slider").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 2000,
+    from: 0,
+    to: 2000,
+    postfix: "px"
+  });
   for(let i=0; i<images.length; i++){
-      container.append("<div class='item'><img alt='" + images[i].fileName + "'src='" + images[i].url + "'></div>");
+      getImageSize(images[i].url, (w, h)=>{
+        console.log(w, h);
+        container.append("<div class='item'>" +
+          "<img alt='" + images[i].fileName + "'src='" + images[i].url + "'>" +
+          "<div class='img-size'>" + w + " X " + h +"</div>" +
+          "</div>");
+      });
   }
 }
 
@@ -31,4 +54,18 @@ function save(key, value) {
 // 从localStorage获取数据
 function get(key) {
   return JSON.parse(localStorage[key]);
+}
+
+// 获取图片大小
+function getImageSize(url, callback) {
+  let img = new Image();
+  img.src = url;
+  // 如果图片被缓存，则直接返回缓存数据
+  if (img.complete) {
+    callback(img.width, img.height);
+  } else {
+    img.onload = function () {
+      callback(img.width, img.height);
+    }
+  }
 }
