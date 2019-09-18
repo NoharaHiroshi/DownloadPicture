65afb5691ff19a2567d574be0b3f2c27c3a09529
Lands <380788433@qq.com>
2019-09-18 19:03:02 +0800

    智能拼图逻辑修改，适配<select>， 将分页的图片用瀑布流的方式加载

--- js/content-script.js
+++ js/content-script.js
@@ -54,11 +54,6 @@ function getImages() {
           width: w,
           height: h
         };
-        fetch(src).then(resp => resp.blob()).then(blob => {
-          let size = blob.size;
-          size = (size / 1024).toFixed(2) + ' kb';
-          imageItem.fielSize = size;
-        });
         imagesInfo.push(imageItem);
       });
     }
@@ -130,36 +125,30 @@ function puzzleImages() {
       '</div>' +
       '</div>');
     let content = $("#puzzleContent");
-    let anaImages = analysisImageUrl();
-    let tmp = 0;
-    let tmpK;
-    for(let k in anaImages){
-      let v = anaImages[k];
-      if(v.length > tmp){
-        tmp = v.length;
-        tmpK = k;
+    regImgSrc((renderImages) => {
+      console.log("callback renderImages: ", renderImages);
+      for(let i in renderImages){
+        console.log("i: ", i);
+        console.log(renderImages[i]);
+        let w = 800;
+        let h = (parseInt(renderImages[i].height) / (parseInt(renderImages[i].width) / w));
+        content.append("<img class='puzzle-img' style='width:"+w+"px;height:"+h+"px;'" +
+          "data-src='" + renderImages[i].url +"'>");
       }
-    }
-    let renderImages = anaImages[tmpK];
-    for(let i in renderImages){
-      let w = 800;
-      let h = (parseInt(renderImages[i].height) / (parseInt(renderImages[i].width) / w));
-      content.append("<img class='puzzle-img' style='width:"+w+"px;height:"+h+"px;'" +
-        "data-src='" + renderImages[i].url +"'>");
-    }
-    let domHeight = content.innerHeight();
-    let renderImg = $(".puzzle-img");
-    lazyLoad(renderImg, domHeight);
-    content.scroll(() => {
-      let scrollTop = content.scrollTop();
-      for(let i=0; i < renderImg.length; i++){
-        if(renderImg[i].offsetTop < scrollTop + domHeight ){
-          renderImg[i].src = renderImg[i].getAttribute('data-src');
+      let domHeight = content.innerHeight();
+      let renderImg = $(".puzzle-img");
+      lazyLoad(renderImg, domHeight);
+      content.scroll(() => {
+        let scrollTop = content.scrollTop();
+        for(let i=0; i < renderImg.length; i++){
+          if(renderImg[i].offsetTop < scrollTop + domHeight ){
+            renderImg[i].src = renderImg[i].getAttribute('data-src');
+          }
         }
-      }
-    });
-    $("#closeImg").click(function(){
-      $("#puzzleImages").remove();
+      });
+      $("#closeImg").click(function(){
+        $("#puzzleImages").remove();
+      });
     });
   }
 }
@@ -172,5 +161,62 @@ function lazyLoad(renderImg, domHeight) {
     if(renderImg[i].offsetTop < scrollTop + domHeight ){
       renderImg[i].src = renderImg[i].getAttribute('data-src');
     }
+  }
+}
+
+// 推算图片地址
+function regImgSrc(callback) {
+  // <select>类型的翻页
+  let selectValue;
+  // 正则替换后的图片地址列表
+  let regImageList = [];
+  let $select = $("option");
+  let images = getImages();
+  console.log($select);
+  if($select.length){
+    for(let i=0; i<$select.length; i++){
+      let selected = $select[i];
+      if(selected.selected){
+        selectValue = selected.value;
+        console.log(selectValue);
+        break;
+      }
+    }
+    let imgReg = new RegExp(selectValue);
+    for(let image of images){
+      if(imgReg.test(image.url)){
+        console.log(image);
+        for(let i=0; i<$select.length; i++){
+          let selected = $select[i];
+          let newImgUrl = image.url.replace(imgReg, selected.value);
+          regImageList.push(newImgUrl);
+        }
+        break;
+      }
+    }
+    console.log("regImageList: ", regImageList);
+    let imagesInfo = {};
+    for(let i=0; i<regImageList.length; i++){
+      let src = regImageList[i];
+      let fileName = src.split("/").pop().split("?")[0];
+      let fileType = fileName.split(".").pop().split("?")[0];
+      getImageSize(src, (w, h)=>{
+        imagesInfo[i] = {
+          url: src,
+          fileName: fileName,
+          fileType: fileType,
+          width: w,
+          height: h
+        };
+      });
+    }
+    console.log("imagesInfo: ", imagesInfo);
+    setTimeout(() => {
+      console.log("imagesInfo.length: ", Object.keys(imagesInfo).length);
+      console.log("regImageList.length: ", regImageList.length);
+      if(Object.keys(imagesInfo).length === regImageList.length){
+        callback(imagesInfo);
+      }
+    }, 500);
   }
 }
